91fb40ce10e4996b43d63720ed8489892fdfa01b
Fábio Sousa <fabiosousa.ce@hotmail.com.br>
2025-08-13 10:15:01 -0300

    severas correções no layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.py6fx.qru"
         minSdk = 25
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.3"
+        versionCode = 4
+        versionName = "0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
